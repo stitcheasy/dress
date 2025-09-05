d99a11fcd58fe90f77875295b4dae97e3cbedb5e
stitcheasy <trythisainow@gmail.com>
2025-09-05 14:22:15 +0530

    Update neck.py

--- pages/neck.py
+++ pages/neck.py
@@ -6,63 +6,88 @@ from readMeasurement import getmeasurement
 from drawline import line
 from drawcurve import curve
 
-from savemultipagePdf import multipagePdf
-from savesinglepagePdf import singlepageA4
+from savemultipagePdf1 import multipagePdf1
+# from savesinglepagePdf import singlepageA4
 
 from altitude import calculate_altitude
 
-def frontneck():
-    smallshift=0.3
-    linewid=0.8
-    sizeoffont=10
-    len=12
-    wid=10
-    
-    data=getmeasurement()
-    fnecksize=data[1][7]
-    bnecksize=data[1][8]
-    
 
+
+smallshift=0.3
+linewid=0.8
+sizeoffont=10
+len=12
+wid=10
+
+data=getmeasurement()
+chest=data[1][1]
+fnecksize=data[1][7]
+bnecksize=data[1][8]
+shoulder=data[1][0]
+neckwid=float(chest) / 12
+
+
+def frontneck():
+    
     with plt.ioff():
         fig,ax=drawgridM(len, wid, 150, smallshift,linewid, sizeoffont)
+        
     
-    shoulder=data[1][0]
-      
-    st.markdown("##### :blue[Shoulder {}]".format(shoulder))
-    measurementStr= 'Front Neck Size ' + str(fnecksize)
-    st.markdown("##### :blue[{}]".format(measurementStr))
+    
 
-    ax.text(5.5, 0.5, '...........cloth fold line..........',color='brown', fontsize=25)
+    ax.text(0.5, 0.2, '.....cloth fold line..........',color='brown', fontsize=25)
+    line(0,0,0, float(shoulder) / 2, '--', 'pink')
+    line(0,0,float(fnecksize) + 2, 0, '--', 'brown')
+
+    #Box
+    line(0.5,0,0.5, neckwid, '--', 'cyan')
+    line(float(fnecksize) + 0.5, 0, float(fnecksize) + 0.5, neckwid,'--', 'cyan')
+    line(0.5,neckwid, float(fnecksize) + 0.5, neckwid,'--', 'cyan')
+
+    #inner mark
+    curve(float(fnecksize)  + 0.5 , 0, 2, neckwid, float(fnecksize) +0.5,neckwid,'--', 'black', "neck inner cut")
+    line(2, neckwid, 0, neckwid,'--', 'black')
+
+    #outer mark
+    NstripWidth=1
+    curve(float(fnecksize)  + 0.5 + NstripWidth , 0, 2 , neckwid + NstripWidth, float(fnecksize) +0.5+ NstripWidth,neckwid+ NstripWidth,'--', 'blue', "neck outer cut")
+    line(2, neckwid+ NstripWidth, 0, neckwid+ NstripWidth,'--', 'blue')
 
     return fig
 
 def backneck():
-    smallshift=0.3
-    linewid=0.8
-    sizeoffont=10
-    len=12
-    wid=10
-    
-    data=getmeasurement()
-    fnecksize=data[1][7]
-    bnecksize=data[1][8]
-
+        
     with plt.ioff():
         fig,ax=drawgridM(len, wid, 150, smallshift,linewid, sizeoffont)
-    
-    shoulder=data[1][0]
-      
-        
-    measurementStr= 'Back Neck Size ' + str(bnecksize)
-    st.markdown("##### :blue[{}]".format(measurementStr))
 
-    ax.text(5.5, 0.5, '...........cloth fold line..........',color='brown', fontsize=25)
+    ax.text(0.5, 0.2, '..cloth fold line..........',color='brown', fontsize=25)
+    line(0,0,0, float(shoulder) / 2, '--', 'pink')
+    line(0,0,float(fnecksize) + 2, 0, '--', 'brown')
+
+    #Box
+    line(0.5,0,0.5, neckwid, '--', 'cyan')
+    line(float(bnecksize) + 0.5, 0, float(bnecksize) + 0.5, neckwid,'--', 'cyan')
+    line(0.5,neckwid, float(bnecksize) + 0.5, neckwid,'--', 'cyan')
+
+    #inner mark
+    curve(float(bnecksize)  + 0.5 , 0, 2, neckwid, float(bnecksize) +0.5,neckwid,'--', 'black', "neck inner cut")
+    line(2, neckwid, 0, neckwid,'--', 'black')
+
+    #outer mark
+    NstripWidth=1
+    curve(float(bnecksize)  + 0.5 + NstripWidth , 0, 2 , neckwid + NstripWidth, float(bnecksize) +0.5+ NstripWidth,neckwid+ NstripWidth,'--', 'blue', "neck outer cut")
+    line(2, neckwid+ NstripWidth, 0, neckwid+ NstripWidth,'--', 'blue')
 
     return fig
 
 
 
 st.markdown("### :rainbow[Indian Style Simple  Kurti - Front and Back Neck Draft]")
+st.markdown("##### :blue[Shoulder {}]".format(shoulder))
+measurementStr= 'Front Neck Size ' + str(fnecksize)
+st.markdown("##### :blue[{}]".format(measurementStr))
+measurementStr2= 'Back Neck Size ' + str(bnecksize)
+st.markdown("##### :blue[{}]".format(measurementStr2))
 
 fig3=frontneck()
 fig4=backneck()
@@ -76,6 +101,8 @@ with col2:
         st.set_page_config(layout="wide")
         st.pyplot(fig4)
 with col3:
-        st.subheader("Download")
-        multipagePdf(fig3)
-        singlepageA4(fig3)
+        st.subheader("Download Pdfs")
+        multipagePdf1(fig3, fname="FrontN.pdf", L="DownloadFrontN", K="FNeck")
+        # singlepageA4(fig3)
+        # st.subheader("Download Back")
+        multipagePdf1(fig4, fname="BackN.pdf", L="DownloadBackN", K="BNeck")
