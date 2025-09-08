33838e303c388c9707460eca61df8d4dd2e96905
stitcheasy <trythisainow@gmail.com>
2025-09-08 13:55:33 +0530

    Update bottom.py

--- pages/bottom.py
+++ pages/bottom.py
@@ -47,34 +47,7 @@ def pant():
     measurementStr= 'Body Measurements : ' + ' Length ' + str(length) + ' ,Shoulder ' + str(shoulder) + ' ,Chest ' + str(chest) + ' ,Waist ' + str(waist) + ' ,Hip ' + str(hip)
     st.markdown("###### :blue[{}]".format(measurementStr))       
 
-    # line(0,0,0,halfshoulder )
-    # line(chestmark,0,chestmark, quarterchest+0.5, '--', 'magenta')
-    # ax.text(chestmark+0.3, 5, 'chest line',rotation=90,color='brown', fontsize=25)
-    # line(waistmark,0,waistmark, quarterwaist+0.5, '--', 'magenta')
-    # ax.text(waistmark+0.3, 5, 'waist line',rotation=90,color='brown', fontsize=25)
-    # line(hipmark,0,hipmark, quarterhip, '--', 'magenta')
-    # ax.text(hipmark+0.3, 5, 'hip line',rotation=90,color='brown', fontsize=25)
-    # line(float(length),0,float(length),quarterhip, '--', 'magenta')
-
-    # line(4,halfshoulder,chestmark, halfshoulder, '--', 'magenta')
-
-    # line(lengthmark,0,lengthmark,quarterhip+1.5, '-', 'blue')
-    # ax.text(lengthmark-2+0.3, 5, 'required length line',rotation=90,color='brown', fontsize=25)
-    # line(waistmark,quarterwaist+0.5,hipmark,quarterhip, '--', 'magenta')
-    # line(waistmark,quarterwaist+0.5+1.5,hipmark,quarterhip+1.5, '-', 'blue')
-    # line(hipmark,quarterhip,lengthmark,quarterhip, '--', 'magenta')
-
-    # line(hipmark,quarterhip+1.5,lengthmark,quarterhip+1.5, '-', 'blue')
-    # curve(chestmark,quarterchest+0.5,waistmark,quarterwaist+0.5, chestmark+3,quarterchest+0.5,'--', 'purple', "fit curve")
-    # curve(chestmark,quarterchest+0.5+1.5,waistmark,quarterwaist+0.5+1.5, chestmark+3,quarterchest+0.5+1.5,'--','blue' ,"curve to cut")
-
-    # curve(4,halfshoulder,chestmark,quarterchest+0.5, chestmark,halfshoulder,'--', 'black', "armhole back side")
-
-    # line(0,halfshoulder,4,halfshoulder-0.3, '-', 'green')
-    # curve(4,halfshoulder-0.3,chestmark,quarterchest+0.5, chestmark+.7,halfshoulder-1,'--', 'green',  "armhole front side")
-
-    # line(0,halfshoulder,4,halfshoulder, '-', 'blue')
-    # line(chestmark,quarterchest+0.5,chestmark,quarterchest+0.5+1.5, '-', 'blue')
+    
     pantclothwid=(float(hip) / 4) + 5
     #line(0,0,float(length) + 2, 0 , 'dotted', 'red')
     mark1=1.5
@@ -104,7 +77,9 @@ def pant():
     line(mark1+mark2+mark3+fold,0, mark1+mark2+mark3+fold,mohri ,'-', 'blue')
     line(mark1+mark2+mark3-fold,mohri, mark1+mark2+mark3+fold,mohri ,'-', 'blue')
 
-    curve(mark1+mark2+mark3-fold,mohri,mark1+mark2+crotchLen - 0.5,quarterbottomwidth + 0.5,mark1+mark2+mark3-(fold*3),mohri+0.5,'-', 'blue',  "cut at blue line")
+    #curve(mark1+mark2+mark3-fold,mohri,mark1+mark2+crotchLen - 0.5,quarterbottomwidth + 0.5,mark1+mark2+mark3-(fold*3),mohri+0.5,'-', 'blue',  "cut at blue line")
+    curve(mark1+mark2+mark3-fold,mohri,mark1+mark2+crotchLen - 0.5,quarterbottomwidth + 0.5,mark1+mark2+mark3-(fold*3),mohri+0.5,'-', 'pink',  "cut choice")
+    line(mark1+mark2+mark3-fold,mohri,mark1+mark2+crotchLen - 0.5,quarterbottomwidth + 0.5 ,'-', 'blue')
 
     line(0,quarterbottomwidth - 2.5, mark1,0 ,'--', 'blue')
     line(mark1,0, mark1+1,quarterbottomwidth - 2.5,'-', 'blue')
@@ -127,3 +102,4 @@ with col1:
         multipagePdf(fig5)
 with col2:
     singlepageA4(fig5)
+
